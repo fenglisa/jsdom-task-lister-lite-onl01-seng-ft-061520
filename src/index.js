8a0b8f68e3656f01e9fecde932fe30b0e376fde0
fenglisa <lisafeng10@gmail.com>
2020-09-18 08:50:24 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit", function(e) {
+    event.preventDefault();
+    const newLi = document.createElement("li");
+    newLi.innerText = document.getElementById("new-task-description").value
+    document.getElementById("tasks").appendChild(newLi);
+    e.target.reset();
+  });
 });
